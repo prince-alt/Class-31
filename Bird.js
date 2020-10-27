e879768bf531e90a5eb20b9f2f25aad42e4843a3
prince-alt <71696898+prince-alt@users.noreply.github.com>
2020-10-27 18:52:06 +0530

    Add files via upload

---
+++ Bird.js
@@ -0,0 +1,31 @@
+class Bird extends BaseClass {
+  constructor(x,y){
+    super(x,y,50,50);
+    this.image = loadImage("sprites/bird.png");
+    this.smokeimage = loadImage("sprites/smoke.png");
+    this.trajectory = [];
+  }
+
+  display() {
+    //this.body.position.x = mouseX;
+    //this.body.position.y = mouseY;
+
+    super.display();
+
+if(this.body.velocity.x>10 && this.body.position.x>200){
+
+
+    var pos = [this.body.position.x,this.body.position.y];
+    
+    this.trajectory.push(pos);
+}
+
+    for(var l = 0;l<this.trajectory.length;l++){
+
+    image(this.smokeimage,this.trajectory[l][0],this.trajectory[l][1]);
+
+    }
+
+
+  }
+}
